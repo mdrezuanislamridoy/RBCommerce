d99de33e4e86f5e383eb028c0cd84445d3606513
mdrezuanislamridoy <mdrezuanislamridoy@gmail.com>
2024-11-11 12:07:29 +0600

    du

--- javascript/index.js
+++ javascript/index.js
@@ -1,21 +1,20 @@
-
-
 const loadComponent = (targetId, componentFilePath) => {
-    fetch(componentFilePath).then(response => response.text()).then(html => {
-        document.getElementById(targetId).innerHTML = html;
+  fetch(componentFilePath)
+    .then((response) => response.text())
+    .then((html) => {
+      document.getElementById(targetId).innerHTML = html;
     });
-}
+};
 
 document.addEventListener("DOMContentLoaded", () => {
-    // loadComponent("header", "header.html");
-    loadComponent("nav", "/components/Navbar.html");
-    loadComponent("carousel", "/components/carousel.html");
-    loadComponent("catagories", "/components/catagories.html");
-    loadComponent("onSale", "/components/onSaleNow.html");
-    loadComponent("newArrival", "/components/NewArraival.html");
-    loadComponent("trending", "/components/trendingProducts.html");
-    loadComponent("contact", "/components/contactUs.html");
-    loadComponent("about", "/components/about.html");
-    loadComponent("trustAndPayment", "/components/trustAndPayment.html");
-    loadComponent("footer", "/components/footer.html");
+  loadComponent("nav", "/components/Navbar.html");
+  loadComponent("carousel", "/components/carousel.html");
+  loadComponent("catagories", "/components/catagories.html");
+  loadComponent("onSale", "/components/onSaleNow.html");
+  loadComponent("newArrival", "/components/NewArraival.html");
+  loadComponent("trending", "/components/trendingProducts.html");
+  loadComponent("contact", "/components/contactUs.html");
+  loadComponent("about", "/components/about.html");
+  loadComponent("trustAndPayment", "/components/trustAndPayment.html");
+  loadComponent("footer", "/components/footer.html");
 });
